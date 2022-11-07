7f23b6751a1b0daee294745e3d11f2e48adfad97
Dang Le <haidang.levn@gmail.com>
2022-11-08 00:29:30 +0200

    Add "routing" function to nav bar
    - Render NavBar inside render(), without importing the component.
    - Add to each nav-item an onClick event, calling the changeViewHandler() that change the current view, acting as an "router" function to the respective page.

--- react-travelplanner/travel-planner/src/App.js
+++ react-travelplanner/travel-planner/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
 import ExploreView from "./views/Explore/ExploreView";
-import NavBar from "./components/NavBar/NavBar";
+import FlightView from "./views/Flight/FlightView";
+import PlannerView from "./views/Planner/PlannerView";
+import ProfileView from "./views/Profile/ProfileView";
+
 
 class App extends Component {
   state = {
     search: "",
     userName: "Homer",
+    view: "flight",
   };
   /* 
   useEffect(()=>{
@@ -19,10 +23,61 @@ class App extends Component {
   };
 
   render() {
+    const viewRender = () => {
+      switch (this.state.view) {
+        case "explore":
+          return <ExploreView></ExploreView>;
+        case "flight":
+          return <FlightView></FlightView>;
+        case "planner":
+          return <PlannerView></PlannerView>;
+        case "profile":
+          return <ProfileView></ProfileView>;
+        default:
+          return <ExploreView></ExploreView>;
+      }
+    };
+    const changeViewHandler = (value) => {
+      this.setState({ view: value });
+      console.log("view changed to ", value);
+    };
     return (
       <div className="App">
-        <ExploreView></ExploreView>
-        <NavBar />
+        {viewRender()}
+        <div className="nav">
+          <div
+            onClick={() => {
+              changeViewHandler("explore");
+            }}
+            className="nav-item active"
+          >
+            Explore
+          </div>
+          <div
+            onClick={() => {
+              changeViewHandler("flight");
+            }}
+            className="nav-item"
+          >
+            Flight
+          </div>
+          <div
+            onClick={() => {
+              changeViewHandler("planner");
+            }}
+            className="nav-item"
+          >
+            Planner
+          </div>
+          <div
+            onClick={() => {
+              changeViewHandler("profile");
+            }}
+            className="nav-item"
+          >
+            Profile
+          </div>
+        </div>
       </div>
     );
   }
