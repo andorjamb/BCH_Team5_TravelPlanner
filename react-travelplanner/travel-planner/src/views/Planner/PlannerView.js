ca71ab63f5f95ff2d3a8b8cdb537214077f932dd
Dang Le <haidang.levn@gmail.com>
2022-11-08 00:32:38 +0200

    Add new view files to views folder
    Provide each views with separated folder contains one CSS and one JS file. These files are copies of the files in the explore view folder earlier.

---
+++ react-travelplanner/travel-planner/src/views/Planner/PlannerView.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./PlannerView.css";
+
+class PlannerView extends Component {
+  render() {
+    return <h1>This is PlannerView</h1>;
+  }
+}
+
+export default PlannerView;
