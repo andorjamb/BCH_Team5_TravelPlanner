60eab67d715bc10b4786078f5354a208a93db4c1
Dang Le <haidang.levn@gmail.com>
2022-11-10 07:29:20 +0200

    Update SearchBar.js with className because eslint complained

--- react-travelplanner/travel-planner/src/components/SearchBar/SearchBar.js
+++ react-travelplanner/travel-planner/src/components/SearchBar/SearchBar.js
@@ -2,9 +2,9 @@ import React from "react";
 // import "./SearchBar.css";
 const SearchBar = () => {
   return (
-    <div class="input-group">
-    <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
-    <button type="button" class="btn btn-outline-primary">search</button>
+    <div className="input-group">
+    <input type="search" className="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+    <button type="button" className="btn btn-outline-primary">search</button>
   </div>
   );
 };
