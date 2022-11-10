295cf07b88737cf20b5d579d74939a61ea5e4623
Jessemwangi <jessejzee.com>
2022-11-10 15:45:43 +0200

    fix image url path

--- react-travelplanner/travel-planner/src/components/Welcome/Welcome.js
+++ react-travelplanner/travel-planner/src/components/Welcome/Welcome.js
@@ -5,12 +5,10 @@ const Welcome = (props) => {
  
   return (
     <div className="welcome">
-      <div id="user-avatar">
-        <img src="https://source.unsplash.com/500x400/?man" alt="user avatar" />
-      </div>
+      
       <div id="user-hello">
        
-          Hello, <br />
+          
           <UserName/>
           
        
