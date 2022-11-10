a85885e2bd8b6309fb605cefdaf46c477e0d54c6
Anna Petelin <adpetelin@gmail.com>
2022-11-10 19:11:26 +0200

    add css color variables to index.css

--- react-travelplanner/travel-planner/src/components/Welcome/Welcome.js
+++ react-travelplanner/travel-planner/src/components/Welcome/Welcome.js
@@ -1,19 +1,21 @@
 import React from "react";
 import "./Welcome.css";
-import {UserName} from '../UserName/UserName'
+import { UserName } from '../UserName/UserName';
+
 const Welcome = (props) => {
- 
+
   return (
+
     <div className="welcome">
       <div id="user-avatar">
         <img src="https://source.unsplash.com/500x400/?man" alt="user avatar" />
       </div>
       <div id="user-hello">
-       
-          Hello, <br />
-          <UserName/>
-          
-       
+
+        Hello, <br />
+        <UserName />
+
+
       </div>
     </div>
   );
