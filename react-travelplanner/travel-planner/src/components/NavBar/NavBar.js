ef4d8b51287029842714e44a768bda97dd3cd6aa
Anna Petelin <adpetelin@gmail.com>
2022-11-10 16:32:54 +0200

    remove doubled up navbar from navbar.js

--- react-travelplanner/travel-planner/src/components/NavBar/NavBar.js
+++ react-travelplanner/travel-planner/src/components/NavBar/NavBar.js
@@ -6,32 +6,14 @@ import "./NavBar.css";
 // import UserAuth from '../../views/Profile/Context/Context'
 
 
-
 const NavBar = (props) => {
-
-
   return (
     <div className="nav">
 
-      <div className="nav-item" onClick={() => {props.changeViewHandler("explore")}}>
-        Explore
-      </div>
-      <div className="nav-item" onClick={() => {props.changeViewHandler("flight")}}>
-        Flight
-      </div>
-      <div className="nav-item" onClick={() => {props.changeViewHandler("planner")}}>
-        Planner
-      </div>
-      <div className="nav-item" onClick={() => {props.changeViewHandler("profile")}}>
-        Profile
-      </div>
-
-      
       <div className="nav-item active"><Link to='/'>Explore</Link></div>
       <div className="nav-item"><Link to='/flight'>Flight</Link> </div>
       <div className="nav-item"><Link to='/planner'>Planner</Link> </div>
-      
-      <div className="nav-item"> <Link to='/profile/signin'>Profile</Link> </div> 
+      <div className="nav-item"> <Link to='/profile/signin'>Profile</Link> </div>
 
     </div>
   );
