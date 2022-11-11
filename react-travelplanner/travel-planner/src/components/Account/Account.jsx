a8e9913cb7847170fb330c02514bd9e49899bf47
Jessemwangi <jessejzee.com>
2022-11-11 20:15:25 +0200

    profile with data

--- react-travelplanner/travel-planner/src/components/Account/Account.jsx
+++ react-travelplanner/travel-planner/src/components/Account/Account.jsx
@@ -1,122 +1,109 @@
 import React from "react";
 import { UserAuth } from "../Context/Context";
-import RecentTrips from "../../views/RecentTrips/RecentTrips"
-import './Account.css';
-import { useState } from 'react';
+import RecentTrips from "../../views/RecentTrips/RecentTrips";
+import "./Account.css";
+import { useState } from "react";
 
-let  htmlString;
-let myplan = [{
-  rU8A3axQNTNlFAvnHhsG6en5qVJ3:{
-    trip4:{
-      name:"names",
-      date:"12/12/2022",
-      imageUrl:"path",
-      rating:4
-    }
-    ,
-    trip2:{
-      name:"names",
-      date:"12/12/2022",
-      imageUrl:"path",
-      rating:4
+let myplan = [
+  {
+    rU8A3axQNTNlFAvnHhsG6en5qVJ3: {
+      trips: [
+        {
+          name: "jesse",
+          date: "12/12/2022",
+          imageUrl: "path",
+          rating: 3,
+        },
+        {
+          name: "limo",
+          date: "12/12/2022",
+          imageUrl: "path",
+          rating: 4,
+        },
+        {
+          name: "peter",
+          date: "12/12/2022",
+          imageUrl: "path",
+          rating: 1,
+        },
+        {
+          name: "tom",
+          date: "12/12/2022",
+          imageUrl: "path",
+          rating: 5,
+        },
+      ],
     },
-    trip6:{
-      name:"names",
-      date:"12/12/2022",
-      imageUrl:"path",
-      rating:4
-    },trip7:{
-      name:"names",
-      date:"12/12/2022",
-      imageUrl:"path",
-      rating:4
-    }
+    rU8A3axQNTNlFAvnHhsG6en5qVJ4: {
+      trip1: {
+        name: "names",
+        date: "12/12/2022",
+        imageUrl: "path",
+        rating: 4,
+      },
+    },
+    rU8A3axQNTNlFAvnHhsG6en5qVJ5: {
+      trip3: {
+        name: "names",
+        date: "12/12/2022",
+        imageUrl: "path",
+        rating: 4,
+      },
+    },
   },
-    rU8A3axQNTNlFAvnHhsG6en5qVJ4:{
-      trip1:{
-        name:"names",
-        date:"12/12/2022",
-        imageUrl:"path",
-        rating:4
-      }
-  },
-  rU8A3axQNTNlFAvnHhsG6en5qVJ5:{
-    trip3:{
-      name:"names",
-      date:"12/12/2022",
-      imageUrl:"path",
-      rating:4
-    }
-}
-}
-]
+];
 
 const Account = () => {
   const [trips, settrips] = useState({});
-    const { logOut, user } = UserAuth();
-    
-  
-    const RecentTrips = () => {
-  
-       const arrtrips = []
-       myplan.forEach(planning =>{
-        arrtrips.push(planning.rU8A3axQNTNlFAvnHhsG6en5qVJ3);
-      
-      });
-      console.log(trips);
-      settrips(arrtrips);
-       htmlString = arrtrips.map((trip) =>{
-        return (
-          <RecentTrips key={trip}
-          trip={trip}
-          name={trip.name}
-          date ={trip.date}
-          imageUrl={trip.imageUrl}
-          rating={trip.rating}/> 
-           );
-        
-      });
-      
-      console.log({htmlString});
-      return {htmlString};
-      };
+  const { logOut, user } = UserAuth();
 
-    const handleSignOut = async () => {
-      try {
-        await logOut();
-      } catch (error) {
-        console.log(error);
-      }
-    };
-  
-    return (
-      
-      <div className="accountContainer">
-        <div>
-        <h2 >
-          Welcome, {user?.displayName}</h2>
-        </div>
-        <div className="profPictureContainer">
-          <img className="profileImage" id="" src="" alt=""/>
-        </div>
-        <div className="signoutButton">
-        <button onClick={handleSignOut} >
-          Logout
-        </button>
-        </div>
-        <div className="tripdetaailsSection">
-          <div className="completedTrips">
-
-          </div>
-          <div className="nextTrips">
-
-          </div>
-
-        </div>
-        {htmlString}
-        {RecentTrips}
-      </div>
-    );
+  const handleSignOut = async () => {
+    try {
+      await logOut();
+    } catch (error) {
+      console.log(error);
+    }
   };
-  
-  export default Account;
+
+  const arrtrips = [];
+
+  if (!user) {
+    return <>Loading</>;
+  }
+
+  const render = myplan.map((item) => (
+    <div key={item[user?.uid] + Math.random()}>
+      <h1>total trips {item[user?.uid]?.trips.length}</h1>
+      {item[user?.uid]?.trips.slice(0, 4).map((plan) => (
+        <RecentTrips
+          key={plan.name}
+          trip={plan}
+          name={plan.name}
+          date={plan.date}
+          imageUrl={plan.imageUrl}
+          rating={plan.rating}
+        />
+      ))}
+    </div>
+  ));
+
+  return (
+    <div className="accountContainer">
+      <div>
+        <h2>Welcome, {user?.displayName}</h2>
+      </div>
+      <div className="profPictureContainer">
+        <img className="profileImage" id="" src={user?.photoURL || 'https://avatars.dicebear.com/v2/avataaars/da67f910f7ac4a0dbeaec3213b5f3d99.svg'} alt="" />
+      </div>
+      <div className="signoutButton">
+        <button onClick={handleSignOut}>Logout</button>
+      </div>
+      <div className="tripdetaailsSection">
+        <div className="completedTrips">{render}</div>
+        <div className="nextTrips"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Account;
--- react-travelplanner/travel-planner/src/views/RecentTrips/RecentTrips.jsx
+++ react-travelplanner/travel-planner/src/views/RecentTrips/RecentTrips.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { useContext, createContext, useEffect, useState } from 'react';
+import React from "react";
+import { useContext, createContext, useEffect, useState } from "react";
 
-
-
- const RecentTrips = (props) => {
-    const [user, setUser] = useState({});
-    return (
-        <div><h1>{props.trip}</h1>
-            <div><p>{props.name}</p></div>
-            <p>{props.date}</p>
-            <p>{props.imageUrl}</p>
-            <p>{props.rating}</p>
-          </div>
-    );
+const RecentTrips = (props) => {
+  return (
+    <div>
+      <h2>recent trips</h2>
+      <div>
+        <p>{props.name}</p>
+      </div>
+      <p>{props.date}</p>
+      <p>{props.imageUrl}</p>
+      <p>{props.rating}</p>
+    </div>
+  );
 };
 
 export default RecentTrips;
