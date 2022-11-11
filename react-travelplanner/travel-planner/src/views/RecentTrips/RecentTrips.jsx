a7824bd18c080dcc00528774d92cf41dd9d95cbd
Jessemwangi <jessejzee.com>
2022-11-12 00:46:07 +0200

    styling the Accounts compnent

--- react-travelplanner/travel-planner/src/views/RecentTrips/RecentTrips.jsx
+++ react-travelplanner/travel-planner/src/views/RecentTrips/RecentTrips.jsx
@@ -1,16 +1,20 @@
 import React from "react";
+import './RecentTrips.css';
 import { useContext, createContext, useEffect, useState } from "react";
 
 const RecentTrips = (props) => {
   return (
-    <div>
-      <h2>recent trips</h2>
-      <div>
-        <p>{props.name}</p>
+    <div className="recentTripContainer">
+      <div className="tripImage"> 
+        <img  src={`${props.imageUrl}.svg`} alt={props.imageUrl} />
       </div>
-      <p>{props.date}</p>
-      <p>{props.imageUrl}</p>
-      <p>{props.rating}</p>
+      <div className="tripdetails">
+        <h3 className="tripname">{props.name}</h3>
+        <p>visited : {props.date}</p>
+        <div><img src={`${props.rating}.svg`} alt={props.rating} /></div>
+      </div>
+      
+ 
     </div>
   );
 };
