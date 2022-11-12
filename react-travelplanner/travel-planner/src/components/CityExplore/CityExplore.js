0d57808c08014f376b114e26d2e5f71b67b46900
Jessemwangi <jessejzee.com>
2022-11-12 14:07:52 +0200

    onclick function for city explore

--- react-travelplanner/travel-planner/src/components/CityExplore/CityExplore.js
+++ react-travelplanner/travel-planner/src/components/CityExplore/CityExplore.js
@@ -8,6 +8,14 @@ class CityExplore extends Component {
     cities: cities,
   };
 
+    handleplanCityTrip = (cityname)=>{
+console.log(cityname);
+localStorage.clear();
+let city ={
+  "SelectedCity":cityname
+}
+localStorage.setItem('city',JSON.stringify(city));
+   }
 //  componentDidMount() //fetch from api, insert into object (wait for data from db first)
 
 
@@ -15,6 +23,7 @@ class CityExplore extends Component {
     const cityArray = this.state.cities.map((city) => {
       return (
         <CityContainer
+        planCityTrip = {() => this.handleplanCityTrip(city.name)}
           key={city.name}
           cityName={city.name.charAt(0).toUpperCase() + city.name.substring(1)}
           description={city.description}
