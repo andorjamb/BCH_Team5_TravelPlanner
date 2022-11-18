a7042ce3ad0775d564ce051535e7f3666cff0554
Dang Le <haidang.levn@gmail.com>
2022-11-18 10:04:31 +0200

    Add Planner layout

--- react-travelplanner/travel-planner/src/views/Planner/PlannerView.js
+++ react-travelplanner/travel-planner/src/views/Planner/PlannerView.js
@@ -5,11 +5,21 @@ import WelcomeUser from "../../components/WelcomeUser/WelcomeUser";
 class PlannerView extends Component {
   render() {
     return (
-      <>
-        <WelcomeUser />
-        <h1>This is PlannerView</h1>
-      </>
-    )
+      <div className="planner-view">
+        {/* <WelcomeUser /> */}
+        <div className="cover-img"></div>
+        <div className="plan-title">
+          <div>
+            <h1>Trip 1</h1>
+            <h2>Add trip dates</h2>
+            <input type="date"></input>
+          </div>
+          <div>
+            <span class="material-symbols-outlined">edit_square</span>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
