1738d65dd297a63a0a9ca4e808384a6be3bd7bda
Dang Le <haidang.levn@gmail.com>
2022-11-21 12:08:08 +0200

    Update Planner View with data fetching

--- react-travelplanner/travel-planner/src/components/SavedPlaces/SavedPlaces.jsx
+++ react-travelplanner/travel-planner/src/components/SavedPlaces/SavedPlaces.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./SavedPlaces.css";
 
-const SavedPlaces = () => {
+const SavedPlaces = (props) => {
   return (
     <div className="saved-places">
-      <img src="https://source.unsplash.com/500x400/?city" alt="" />
-      <span className="saved-places-name">Place 1</span>
+      <img src={`https://source.unsplash.com/500x400/?${props.name}`} alt="" />
+      <span className="saved-places-name">{props.name}</span>
       <span className="material-symbols-outlined add">add_circle</span>
     </div>
   );
--- react-travelplanner/travel-planner/src/views/Planner/PlannerView.jsx
+++ react-travelplanner/travel-planner/src/views/Planner/PlannerView.jsx
@@ -2,9 +2,31 @@ import React, { Component } from "react";
 import "./PlannerView.css";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import SavedPlaces from "../../components/SavedPlaces/SavedPlaces";
+import { db } from "../../FireBaseInit";
+import { collection, getDocs } from "firebase/firestore";
+
 
 class PlannerView extends Component {
+  state = {
+    sights: [],
+  };
+  sightData = [];
+  componentDidMount = async () => {
+    const querySnapshot = await getDocs(collection(db, "sights"));
+
+    querySnapshot.docs.forEach((sight) => {
+      this.sightData.push(sight.data());
+    });
+    this.setState({
+      sights: this.sightData,
+    });
+    console.log("Data from CDM: ", this.sightData[0]);
+  };
   render() {
+    const sightList = this.state.sights.map((sight) => (
+      <SavedPlaces key={sight.sightName} name={sight.sightName} rating={sight.rating} />
+    ));
+    console.log(sightList)
     return (
       <div className="planner-view">
         <div className="cover-img"></div>
@@ -26,11 +48,7 @@ class PlannerView extends Component {
 
           <h2>Saved places</h2>
           <div className="saved-places-wrapper">
-            <SavedPlaces />
-            <SavedPlaces />
-            <SavedPlaces />
-            <SavedPlaces />
-            <SavedPlaces />
+            {sightList}
           </div>
           <h1>Notes</h1>
           <textarea
