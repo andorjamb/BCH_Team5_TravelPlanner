ce9987ad447d34f4319dbc9c19d3f0263740fb3a
Dang Le <haidang.levn@gmail.com>
2022-11-24 11:47:10 +0200

    update plannerview with new component

--- react-travelplanner/travel-planner/src/views/Planner/PlannerView.jsx
+++ react-travelplanner/travel-planner/src/views/Planner/PlannerView.jsx
@@ -2,31 +2,60 @@ import React, { Component } from "react";
 import "./PlannerView.css";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import SavedPlaces from "../../components/SavedPlaces/SavedPlaces";
+import Trip from "../../components/Trip/Trip";
 import { db } from "../../FireBaseInit";
 import { collection, getDocs } from "firebase/firestore";
-
 
 class PlannerView extends Component {
   state = {
     sights: [],
+    trip: [],
   };
-  sightData = [];
+  sightsData = [];
+  tripData = [];
   componentDidMount = async () => {
     const querySnapshot = await getDocs(collection(db, "sights"));
 
     querySnapshot.docs.forEach((sight) => {
-      this.sightData.push(sight.data());
+      this.sightsData.push(sight.data());
     });
     this.setState({
-      sights: this.sightData,
+      sights: this.sightsData,
     });
-    console.log("Data from CDM: ", this.sightData[0]);
   };
-  render() {
-    const sightList = this.state.sights.map((sight) => (
-      <SavedPlaces key={sight.sightName} name={sight.sightName} rating={sight.rating} />
+  addTrip = (sightName, cityName) => {
+    console.log(`Adding ${sightName} to trip`);
+    this.tripData.push({cityName:cityName, sightName:sightName});
+    console.log("New data added to tripData: ", this.tripData)
+    this.setState({
+      trip: this.tripData,
+    });
+  };
+  renderSightList = () => {
+    let sightList = this.state.sights.map((sight) => (
+      <SavedPlaces
+        key={sight.sightName}
+        sightName={sight.sightName}
+        cityName={sight.cityName}
+        addTrip={this.addTrip}
+      />
     ));
-    console.log(sightList)
+    return sightList;
+  };
+  renderTripList = () => {
+    let tripList = this.state.trip.map((trip,index) => (
+      <Trip
+        index={index+1}
+        key={trip.sightName}
+        sightName={trip.sightName}
+        cityName={trip.cityName}
+        addTrip={this.addTrip}
+      />
+    ));
+    return tripList;
+  };
+
+  render() {
     return (
       <div className="planner-view">
         <div className="cover-img"></div>
@@ -34,7 +63,7 @@ class PlannerView extends Component {
           <div className="plan-info">
             <h1>Trip 1</h1>
             <div>
-              <span class="material-symbols-outlined">calendar_month</span>{" "}
+              <span class="material-symbols-outlined">calendar_month</span>
               <input type="date" placeholder="add dates"></input>
             </div>
           </div>
@@ -45,11 +74,13 @@ class PlannerView extends Component {
         <div className="plan-content">
           <h1>Places to visit</h1>
           <SearchBar />
+          <h2>Search result</h2>
 
           <h2>Saved places</h2>
-          <div className="saved-places-wrapper">
-            {sightList}
-          </div>
+          <div className="saved-places-wrapper">{this.renderSightList()}</div>
+          <h2>Trip details</h2>
+          {this.renderTripList()}
+
           <h1>Notes</h1>
           <textarea
             className="plan-note"
