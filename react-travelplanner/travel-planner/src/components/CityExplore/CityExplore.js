0b3e66784a774a8729460c93140c19da1c1aeebd
Anna Petelin <andorjamb@protonmail.com>
2022-11-25 14:30:51 +0200

    add database data to cityExplore, temporarily delete account jsx component

--- react-travelplanner/travel-planner/src/components/CityExplore/CityExplore.js
+++ react-travelplanner/travel-planner/src/components/CityExplore/CityExplore.js
@@ -1,64 +1,111 @@
-import React, { Component, createElement } from "react";
+import React, { Component } from "react";
 import "./CityExplore.css";
 import CityContainer from "../CityContainer/CityContainer";
-import {cities} from "../../data/cities.js";
+//import { cities } from "../../data/cities.js";
 import { db } from '../../FireBaseInit';
 import {
   addDoc,
   serverTimestamp, collection, getDocs, onSnapshot, where, setLoading,
   doc, query, orderBy, limit, deleteDoc, setDoc, updateDoc
 } from "@firebase/firestore";
+import { async } from "@firebase/util";
 
-let cityName;
+const citiesRef = collection(db, "cities");
+//const sightsRef = collection(db, "sights");
 
-const urls = [`https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exchars=400&explaintext&titles=${cityName}`, ``]
+function makeNewRand() { return (Math.floor(Math.random() * 10)) };
+let randNumbers = [];
 
+
+
+/**///////////////////////////////////////////////// */
 
 class CityExplore extends Component {
   state = {
-    cities: cities,
+    //sights: [];
+    currentRand: undefined,
+    newRand: undefined,
+    displayCities: [],
+    cityData: []
   }
 
-ratingStars = (r)=>{
-  let grade= '';
-  let i = 0;
-  while (i<r){
-    grade = grade + 'grade';
-    i++;
-  }
-  return(grade)
+  cityData = [];
 
-   } 
-
- handleplanCityTrip = (cityname)=>{
-  console.log(cityname);
-  localStorage.clear();
-  let city ={
-    "SelectedCity":cityname
+  randArray = () => {
+    while (randNumbers.length < 4) {
+      let newRandom = makeNewRand();
+      if (randNumbers.includes(newRandom)) { makeNewRand(); }
+      else {
+        this.setState({ currentRand: newRandom });
+        randNumbers.push(newRandom);
+      }
+    }
   }
-  localStorage.setItem('city',JSON.stringify(city));
-}
+
+  /*   randomCities = () => {
+      let displayCities = [];
+      if (randNumbers !== []) {
+        for (var i of randNumbers) {
+          displayCities.push(this.state.cityData[i])
+        }
   
-/*   
-  componentDidMount(){
+      } console.log(displayCities);
+      this.setState({ displayCities: displayCities });
+    } */
 
-    let url = 'http://localhost:8080/cities/descriptions/helsinki';
-    let opts = {'mode': 'no-cors'}
-    fetch(url, opts)
-    .then((response) => {
-      console.log(response)});
+  //const sightsDocs = await getDocs(query(sightsRef, where("cityName", "==", city)));
+
+  ratingStars = (r) => {
+    let grade = '';
+    let i = 0;
+    while (i < r) {
+      grade += 'grade';
+      i++;
+    }
+    return (grade);
   }
- */
-  render() {
-    const cityArray = this.state.cities.map((city) => {
-      return (<CityContainer
-        planCityTrip = {() => this.handleplanCityTrip(city.name)}
-          key={city.name}
-          cityName={city.name.charAt(0).toUpperCase() + city.name.substring(1)}
-          description={city.description}
-          rating={this.ratingStars(city.rating)}
 
-        />
+  /*   handleplanCityTrip = (cityname) => {
+      console.log(cityname);
+      localStorage.clear();
+      let city = {
+        "SelectedCity": cityname
+      }
+      localStorage.setItem('city', JSON.stringify(city));
+    } */
+
+
+  componentDidMount = async () => {
+    const querySnapshot = await getDocs(collection(db, "cities"));
+    querySnapshot.docs.forEach((city) => {
+      this.cityData.push(city.data());
+    });
+    this.setState({
+      cityData: this.cityData,
+    });
+
+    console.log(this.state.cityData);
+    this.setState({ currentRand: makeNewRand() });
+    this.randArray();
+    // this.randomCities();
+
+  };
+
+  /*  let url = 'http://localhost:8080/cities/descriptions/helsinki';
+   let opts = {'mode': 'no-cors'}
+   fetch(url, opts)
+   .then((response) => {
+     console.log(response)}); }*/
+
+  render() {
+    const cityArray = this.state.cityData.map((city) => {
+      return (<CityContainer
+        // planCityTrip={() => this.handleplanCityTrip(city.cityName)}
+        key={city.cityName}
+        cityName={city.cityName.charAt(0).toUpperCase() + city.cityName.substring(1)}
+        description={city.description}
+        rating={this.ratingStars(city.rating)}
+      />
       );
     });
 
