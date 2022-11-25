6d2857c07fe46b01bfd888a121e10700298b1229
Dang Le <haidang.levn@gmail.com>
2022-11-25 17:27:43 +0200

    Update PlannerView
    - The whole page is wrap in a <form> tag, so click "save" button submits and post the data to collection "tripDemo"
    - searchbar search by city name or sight name from data in collection "sights", then show sights in Search Results

--- react-travelplanner/travel-planner/src/views/Planner/PlannerView.jsx
+++ react-travelplanner/travel-planner/src/views/Planner/PlannerView.jsx
@@ -4,12 +4,18 @@ import SearchBar from "../../components/SearchBar/SearchBar";
 import SavedPlaces from "../../components/SavedPlaces/SavedPlaces";
 import Trip from "../../components/Trip/Trip";
 import { db } from "../../FireBaseInit";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, addDoc } from "firebase/firestore";
+import { async } from "@firebase/util";
 
 class PlannerView extends Component {
   state = {
+    userID: 1,
     sights: [],
     trip: [],
+    tripName: "New Trip Name",
+    tripDate: "",
+    search: "",
+    notes: "",
   };
   sightsData = [];
   tripData = [];
@@ -24,28 +30,23 @@ class PlannerView extends Component {
     });
   };
   addTrip = (sightName, cityName) => {
-    console.log(`Adding ${sightName} to trip`);
-    this.tripData.push({cityName:cityName, sightName:sightName});
-    console.log("New data added to tripData: ", this.tripData)
+    // Push the clicked sight into another array, then set them into "trip" state
+    this.tripData.push({ cityName: cityName, sightName: sightName });
+    console.log("New data added to tripData: ", this.tripData);
     this.setState({
       trip: this.tripData,
     });
+    // Filter out the click sight from search result
+    this.sightsData = this.sightsData.filter((x) => x.sightName !== sightName);
+    this.setState({
+      sights: this.sightsData,
+    });
   };
-  renderSightList = () => {
-    let sightList = this.state.sights.map((sight) => (
-      <SavedPlaces
-        key={sight.sightName}
-        sightName={sight.sightName}
-        cityName={sight.cityName}
-        addTrip={this.addTrip}
-      />
-    ));
-    return sightList;
-  };
+
   renderTripList = () => {
-    let tripList = this.state.trip.map((trip,index) => (
+    let tripList = this.state.trip.map((trip, index) => (
       <Trip
-        index={index+1}
+        index={index + 1}
         key={trip.sightName}
         sightName={trip.sightName}
         cityName={trip.cityName}
@@ -55,44 +56,123 @@ class PlannerView extends Component {
     return tripList;
   };
 
+  searchHandler = (e) => {
+    console.log("Searching for ", e.target.value);
+    this.setState({ search: e.target.value });
+  };
+
+  onChangeHandler = (e, itemToChange) => {
+    if (itemToChange === "tripName") {
+      this.setState({
+        tripName: e.target.value,
+      });
+    }
+    if (itemToChange === "tripDate") {
+      this.setState({
+        tripDate: e.target.value,
+      });
+    }
+    if (itemToChange === "notes") {
+      this.setState({
+        notes: e.target.value,
+      });
+    }
+  };
+
+  submitHandler = async (e) => {
+    e.preventDefault();
+    // get Date data for created day of the plan
+    let today = new Date();
+    let date =
+      today.getFullYear() +
+      "-" +
+      (today.getMonth() + 1) +
+      "-" +
+      today.getDate();
+    const dataToSubmit = {
+      tripName: this.state.tripName,
+      tripDate: this.state.tripDate,
+      dayCreated: date,
+      sights: this.state.trip,
+      notes: this.state.notes,
+      userID: this.state.userID,
+    };
+    console.log("Sending this data to firebase: -->>> ", dataToSubmit)
+    await addDoc(collection(db, "tripDemo"), dataToSubmit);
+    console.log("Add data successful!!!");
+  };
+
   render() {
+    // search by both sight name and city name
+    const sightFilter = this.state.sights.filter((sight) => {
+      return (
+        sight.sightName
+          .toLowerCase()
+          .includes(this.state.search.toLowerCase()) ||
+        sight.cityName.toLowerCase().includes(this.state.search.toLowerCase())
+      );
+    });
+
+    const renderSightList = sightFilter.map((sight) => (
+      <SavedPlaces
+        key={sight.sightName}
+        sightName={sight.sightName}
+        cityName={sight.cityName}
+        addTrip={this.addTrip}
+      />
+    ));
+
     return (
       <div className="planner-view">
         <div className="cover-img"></div>
-        <div className="plan-title">
-          <div className="plan-info">
-            <h1>Trip 1</h1>
-            <div>
-              <span class="material-symbols-outlined">calendar_month</span>
-              <input type="date" placeholder="add dates"></input>
+        <form onSubmit={this.submitHandler}>
+          <div className="plan-title">
+            <div className="plan-info">
+              <h1>Plan Details</h1>
+              <label>Trip Name:  </label>
+              <input
+                type="text"
+                placeholder={this.state.tripName}
+                onChange={(e) => this.onChangeHandler(e, "tripName")}
+              />
+              <div>
+                <span className="material-symbols-outlined">
+                  calendar_month
+                </span>
+                <input
+                  type="date"
+                  placeholder="add trip dates"
+                  onChange={(e) => this.onChangeHandler(e, "tripDate")}
+                ></input>
+              </div>
+            </div>
+           
+          </div>
+          <div className="plan-content">
+            <h1>Places to visit</h1>
+            <SearchBar searchHandler={this.searchHandler} />
+
+            <h2>Search results</h2>
+            <div className="saved-places-wrapper">{renderSightList}</div>
+            <h2>Trip details</h2>
+            {this.renderTripList()}
+
+            <h1>Notes</h1>
+            <textarea
+              className="plan-note"
+              cols="50"
+              rows="10"
+              placeholder="Write anything here: tips and tricks, how to get around, budgeting, etc."
+              onChange={(e) => this.onChangeHandler(e, "notes")}
+            ></textarea>
+            <div className="plan-button">
+              <button className="delete">Delete</button>
+              <button type={"submit"} className="save">
+                Save
+              </button>
             </div>
           </div>
-          <div className="edit-button">
-            <span class="material-symbols-outlined">edit_square</span>
-          </div>
-        </div>
-        <div className="plan-content">
-          <h1>Places to visit</h1>
-          <SearchBar />
-          <h2>Search result</h2>
-
-          <h2>Saved places</h2>
-          <div className="saved-places-wrapper">{this.renderSightList()}</div>
-          <h2>Trip details</h2>
-          {this.renderTripList()}
-
-          <h1>Notes</h1>
-          <textarea
-            className="plan-note"
-            cols="50"
-            rows="10"
-            placeholder="Write anything here: tips and tricks, how to get around, budgeting, etc."
-          ></textarea>
-          <div className="plan-button">
-            <button className="delete">Delete</button>
-            <button className="save">Save</button>
-          </div>
-        </div>
+        </form>
       </div>
     );
   }
