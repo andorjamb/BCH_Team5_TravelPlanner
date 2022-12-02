45ea3da9516e71cd0e23fc79daf6c6c0be0a971b
Dang Le <haidang.levn@gmail.com>
2022-12-02 15:34:17 +0200

    Update planner view with the right data format
    Data is now in the final format. UID is now hardcoded and should be replaced afterward.
    - Delete button reload the page
    - Save button send the data to db and navigate to thankyou page

--- react-travelplanner/travel-planner/src/views/Planner/PlannerView.jsx
+++ react-travelplanner/travel-planner/src/views/Planner/PlannerView.jsx
@@ -4,12 +4,23 @@ import SearchBar from "../../components/SearchBar/SearchBar";
 import SavedPlaces from "../../components/SavedPlaces/SavedPlaces";
 import Trip from "../../components/Trip/Trip";
 import { db } from "../../FireBaseInit";
-import { collection, getDocs, addDoc } from "firebase/firestore";
-import { async } from "@firebase/util";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  serverTimestamp,
+} from "firebase/firestore";
+import { Link, Navigate } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 
+/* UID for everyone:
+- Jesse: OG04lGkSWibiXstJ4zWFdh92w8J2
+- Anna: adPz97i9O6N4WOxE467OFMhKwgC3
+- Dang: 4paHqkOpZoWosQCMZaDHKNmA3GK2
+ */
 class PlannerView extends Component {
   state = {
-    userID: 1,
+    userID: "4paHqkOpZoWosQCMZaDHKNmA3GK2",
     sights: [],
     trip: [],
     tripName: "New Trip Name",
@@ -44,6 +55,11 @@ class PlannerView extends Component {
   };
 
   renderTripList = () => {
+    if (this.state.trip.length == 0) {
+      return (
+        <p>Add destinations to your trip from the list below ( ͡❛ ‿‿ ͡❛) </p>
+      );
+    }
     let tripList = this.state.trip.map((trip, index) => (
       <Trip
         index={index + 1}
@@ -79,44 +95,46 @@ class PlannerView extends Component {
     }
   };
 
+  resetHandler = () => {
+    window.location.reload();
+  };
+
   submitHandler = async (e) => {
-    e.preventDefault();
-    // get Date data for created day of the plan
-    let today = new Date();
-    let date =
-      today.getFullYear() +
-      "-" +
-      (today.getMonth() + 1) +
-      "-" +
-      today.getDate();
+    /*     e.preventDefault(); this prevent the link to be active, and also run the onclick event.
+     */
     const dataToSubmit = {
       tripName: this.state.tripName,
       tripDate: this.state.tripDate,
-      dayCreated: date,
+      dayCreated: serverTimestamp(),
+      dayUpdated: serverTimestamp(),
       sights: this.state.trip,
       notes: this.state.notes,
       userID: this.state.userID,
+      transactionID: uuidv4(),
     };
-    console.log("Sending this data to firebase: -->>> ", dataToSubmit)
-    await addDoc(collection(db, "tripDemo"), dataToSubmit);
+    console.log("Sending this data to firebase: -->>> ", dataToSubmit);
+
+    // change collection of firebase db here
+    await addDoc(collection(db, "usersTrip"), dataToSubmit);
     console.log("Add data successful!!!");
+    console.log("redirect to plannerview .....");
+    return <Navigate to="/thankyou"></Navigate>;
   };
 
   render() {
-
+    console.log("test ", this.props.navigation);
     // search by both sight name and city name
     const sightFilter = this.state.sights.filter((sight) => {
       return (
-
-        sight.sightName?.toLowerCase().includes(this.state.search.toLowerCase()) ||
+        sight.sightName
+          ?.toLowerCase()
+          .includes(this.state.search.toLowerCase()) ||
         sight.cityName?.toLowerCase().includes(this.state.search.toLowerCase())
-      )
-    }
-    );
-
+      );
+    });
 
     const renderSightList = sightFilter.map((sight) => (
-      < SavedPlaces
+      <SavedPlaces
         key={sight.sightName}
         sightName={sight.sightName}
         cityName={sight.cityName}
@@ -132,7 +150,7 @@ class PlannerView extends Component {
           <div className="plan-title">
             <div className="plan-info">
               <h1>Plan Details</h1>
-              <label>Trip Name:  </label>
+              <label>Trip Name: </label>
               <input
                 type="text"
                 placeholder={this.state.tripName}
@@ -148,20 +166,18 @@ class PlannerView extends Component {
                   onChange={(e) => this.onChangeHandler(e, "tripDate")}
                 ></input>
               </div>
-
             </div>
-
           </div>
 
           <div className="plan-content">
-            <h1>Places to visit</h1>
-            <SearchBar searchHandler={this.searchHandler} />
-
-
-            <h2>Search results</h2>
-            <div className="saved-places-wrapper">{renderSightList}</div>
-            <h2>Trip details</h2>
+            <p>Active UID: {this.state.userID}</p>
+            <h1>Trip details</h1>
             {this.renderTripList()}
+
+            <h1>Places to visit</h1>
+            <SearchBar searchEvent={this.searchHandler} />
+
+            <div className="saved-places-wrapper">{renderSightList}</div>
 
             <h1>Notes</h1>
             <textarea
@@ -172,10 +188,18 @@ class PlannerView extends Component {
               onChange={(e) => this.onChangeHandler(e, "notes")}
             ></textarea>
             <div className="plan-button">
-              <button className="delete">Delete</button>
-              <button type={"submit"} className="save">
-                Save
+              <button
+                type="button"
+                className="delete"
+                onClick={this.resetHandler}
+              >
+                Delete
               </button>
+              <Link to="/thankyou" onClick={this.submitHandler}>
+                <button type="submit" className="save">
+                  Save
+                </button>
+              </Link>
             </div>
           </div>
         </form>
